4325f67c85004a33e1eb872b60405bb5c80d1133
amadhav1 <amadhav1@jhu.edu>
2017-06-08 16:13:34 -0400

    added logo and list of pages

--- footer.php
+++ footer.php
@@ -2,8 +2,57 @@
   </div> <!-- /container -->
       <footer class="footer">
       <div class="container">
-        <p class="text-muted"> ©<?php bloginfo('name');?> 2016 - <?php bloginfo('description');?></p>
-      </div>
+		<div class="footer-identification pull-left">
+		
+		<a class="footer__logo" href="//www.jhu.edu">
+<?php if( get_field('jhu_logo', 'option') ): ?>
+
+	<img src="<?php the_field('jhu_logo', 'option'); ?>" />
+
+<?php endif; ?>
+
+</a>
+			<p class="copy">©Johns Hopkins University</p>
+			<p class="copy">Baltimore, Maryland 21218</p>
+			<p class="copy">All rights reserved</p>
+		</div>
+	    <div class="footer-menu pull-right">
+   	 		  	<?php
+if ( is_user_logged_in() &&  !current_user_can( 'manage_options' ) ) {
+  
+    $menu_footer='logged-in-users';
+	
+
+}
+elseif ( is_user_logged_in() &&  current_user_can( 'manage_options' ) ) {
+     $menu_footer= 'logged-in-board';
+	}
+else { 
+    $menu_footer= 'logged-out-users';
+	
+}
+?>
+
+
+	 <?php $foot_args = array(
+	 
+	  'menu' => $menu_footer,
+  'container'       => false,
+  'echo'            => false,
+  'link_before'       => '<p>',
+  'link_after'        => '</p>',
+  'items_wrap'      => '%3$s',
+    'depth'           => 1,
+  
+);
+?>
+ 
+<?php echo strip_tags(wp_nav_menu( $foot_args ), '<a>, <p>' );
+
+
+?>
+	</div>
+  </div>
     </footer>
     
   
